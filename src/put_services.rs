7651cfd24ef369e248e97c7e31545437fa3cfea4
clementreiffers <reiffersclement@gmail.com>
2023-06-27 00:23:42 +0200

    recept of all values for new_secret done, need to instantiate the secret inside kubernetes now

--- src/put_services.rs
+++ src/put_services.rs
@@ -1,8 +1,12 @@
+use std::collections::BTreeMap;
+
 use actix_multipart::{Field, Multipart};
-use actix_web::web::{BytesMut, Json, Path};
+use actix_web::web::{Bytes, BytesMut, Path};
 use actix_web::{put, Error, HttpResponse};
 use futures::StreamExt;
+use k8s_openapi::apimachinery::pkg::apis::meta::v1::ObjectMeta;
 use regex::Regex;
+use serde::{Deserialize, Serialize};
 use serde_json::{json, Value};
 
 use crate::fs::read_json;
@@ -17,7 +21,7 @@ fn get_filename_from_field(field: &Field) -> &str {
 
 fn is_correct_filename(filename: &String) -> bool {
     let regex = Regex::new(r#"(?i)\.(mjs|js|wasm)$"#).unwrap();
-    filename != "" && regex.is_match(filename.as_str())
+    !filename.is_empty() && regex.is_match(filename.as_str())
 }
 
 async fn get_file_content(field: &mut Field) -> BytesMut {
@@ -52,7 +56,7 @@ pub async fn save_file(
     )))
 }
 
-fn generate_new_secret_message(secret_name: String, success: bool) -> serde_json::Value {
+fn generate_new_secret_message(secret_name: &String, success: bool) -> serde_json::Value {
     json!({
       "result": {
         "name": secret_name,
@@ -64,10 +68,40 @@ fn generate_new_secret_message(secret_name: String, success: bool) -> serde_json
     })
 }
 
-#[put("/client/v4/accounts/{accounts}/workers/scripts/{scripts}/secrets")]
-pub async fn new_secret() -> Result<HttpResponse, Error> {
-    let secret_name = "TEST".to_owned();
+fn create_secret(
+    name: &String,
+    text: String,
+    secret_type: &String,
+) -> k8s_openapi::api::core::v1::Secret {
+    let mut string_data = BTreeMap::new();
+    string_data.insert(name.into(), text);
+    k8s_openapi::api::core::v1::Secret {
+        metadata: ObjectMeta {
+            name: Some(name.into()),
+            ..Default::default()
+        },
+        string_data: Some(string_data),
+        type_: Some(secret_type.into()),
+        ..Default::default()
+    }
+}
 
-    let message = generate_new_secret_message(secret_name, true);
+#[derive(Serialize, Deserialize, Debug)]
+struct Data {
+    name: String,
+    text: String,
+    #[serde(rename = "type")]
+    type_: String,
+}
+
+#[put("/client/v4/accounts/{accounts}/workers/scripts/{scripts}/secrets")]
+pub async fn new_secret(path: Path<(String, String)>, bytes: Bytes) -> Result<HttpResponse, Error> {
+    let (accounts, scripts) = path.into_inner();
+    let mut text = String::from_utf8(bytes.to_vec()).expect("failed to cast Bytes to String");
+    let values: Data = serde_json::from_str(text.as_mut_str()).expect("failed to deserialize");
+    println!("{}", values.type_);
+    //let secret = create_secret(&name, text, &secret_type);
+    let name = "sg".to_string();
+    let message = generate_new_secret_message(&name, true);
     Ok(HttpResponse::Ok().body(message.to_string()))
 }
