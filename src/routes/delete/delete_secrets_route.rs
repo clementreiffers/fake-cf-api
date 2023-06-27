ed53d4cb40386f660d47e5e364595602385fc2ed
clementreiffers <reiffersclement@gmail.com>
2023-06-27 15:15:33 +0200

    update secrets deletions

--- src/routes/delete/delete_secrets_route.rs
+++ src/routes/delete/delete_secrets_route.rs
@@ -10,25 +10,8 @@ use crate::routes::kube::create_kube_client;
 #[delete("/client/v4/accounts/{accounts}/workers/scripts/{scripts}/secrets/{secrets}")]
 pub async fn delete_secrets(path: web::Path<(String, String, String)>) -> HttpResponse {
     let (accounts, scripts, secrets) = path.into_inner();
+    let secret_name = format!("{}.{}.{}", accounts, scripts, secrets);
 
-    /*    let client = create_kube_client().await;
-    let label_selector = format!("accounts={},scripts={}", accounts, scripts);
-    let list_params = ListParams::default().labels(&label_selector);
-    let kube_secrets: Api<k8s_openapi::api::core::v1::Secret> = Api::all(client);
-    let secret_list = kube_secrets.list(&list_params).await;
-    let delete_params = DeleteParams::default();
-
-    if let Ok(secret) = secret_list {
-        for items in secret.items {
-            let name = items.metadata.name.expect("failed to get name");
-            if name == secrets {
-                match kube_secrets.delete("aaa", &delete_params).await {
-                    Ok(_) => println!("deleted"),
-                    Err(err) => eprintln!("failed to delete: {}", err),
-                }
-            }
-        }
-    }*/
     let client = create_kube_client().await;
     let kube_secret: Api<Secret> = Api::namespaced(client.clone(), "default");
     let label_selector = format!("accounts={},scripts={}", accounts, scripts);
@@ -40,7 +23,7 @@ pub async fn delete_secrets(path: web::Path<(String, String, String)>) -> HttpRe
 
     for items in secrets_list.items {
         let name = items.metadata.name.expect("failed to get name");
-        if name == secrets {
+        if name == secret_name {
             let dp = DeleteParams::default();
             kube_secret
                 .delete(&*name, &dp)
